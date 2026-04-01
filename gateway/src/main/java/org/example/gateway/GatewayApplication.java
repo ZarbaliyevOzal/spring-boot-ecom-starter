b2d20360c4d51ba3962d6133639f9ab2a6e2c7bb
Ozal Zarbaliyev <zarbaliyevozal@gmail.com>
2026-04-01 13:52:09 +0400

    implemented keycloak in gateway

--- gateway/src/main/java/org/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/example/gateway/GatewayApplication.java
@@ -3,6 +3,9 @@ package org.example.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,14 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("path_route",
+						r -> r.path("/api/v1/users/**").uri("http://localhost:10010"))
+				.build();
 	}
 
 }
---
+++ gateway/src/main/java/org/example/gateway/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package org.example.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+//import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                        .pathMatchers("/", "/actuator/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+//    @Bean
+//    public ReactiveJwtDecoder reactiveJwtDecoder() {
+//        return NimbusReactiveJwtDecoder
+//                .withJwkSetUri("http://localhost:9090/realms/myrealm/protocol/openid-connect/certs")
+//                .build();
+//    }
+}
