99bb600799a53d5abad3c6ddff4e6379f6b88557
Ozal Zarbaliyev <zarbaliyevozal@gmail.com>
2026-04-06 13:05:06 +0400

    implemented keycloak in product service

---
+++ product-service/src/main/java/org/example/product_service/config/KeycloakJwtConverter.java
@@ -0,0 +1,53 @@
+package org.example.product_service.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class KeycloakJwtConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        // 🔹 Realm roles
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+        if (realmAccess != null && realmAccess.get("roles") instanceof List<?>) {
+            List<?> roles = (List<?>) realmAccess.get("roles");
+
+            for (Object role : roles) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + role.toString()));
+            }
+        }
+
+        // 🔹 Client roles
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        if (resourceAccess != null && resourceAccess.get("ecom_starter") instanceof Map) {
+
+            Map<?, ?> client = (Map<?, ?>) resourceAccess.get("ecom_starter");
+
+            if (client.get("roles") instanceof List<?>) {
+                List<?> roles = (List<?>) client.get("roles");
+
+                for (Object role : roles) {
+                    authorities.add(new SimpleGrantedAuthority("ROLE_" + role.toString()));
+                }
+            }
+        }
+
+//        System.out.println("authorities: " + authorities);
+
+        return new JwtAuthenticationToken(jwt, authorities);
+    }
+}
---
+++ product-service/src/main/java/org/example/product_service/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package org.example.product_service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final KeycloakJwtConverter keycloakJwtConverter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(
+                                "/",
+                                "/actuator/**"
+                        )
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth -> oauth
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(keycloakJwtConverter))
+                )
+                .build();
+    }
+}
