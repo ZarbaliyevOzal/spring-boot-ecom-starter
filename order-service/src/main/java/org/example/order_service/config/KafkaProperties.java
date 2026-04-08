15e54ae74c2fb6c94793d615f63bd5ea17de3c54
Ozal Zarbaliyev <zarbaliyevozal@gmail.com>
2026-04-08 11:58:10 +0400

    implement kafka in order service

---
+++ order-service/src/main/java/org/example/order_service/config/KafkaProperties.java
@@ -0,0 +1,24 @@
+package org.example.order_service.config;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.Map;
+
+@Validated
+@ConfigurationProperties(prefix = "app.kafka")
+public record KafkaProperties (
+    @Valid
+    Map<String, TopicConfig> topics
+) {
+    public record TopicConfig(
+        @NotBlank String name,
+        @Min(1) int partition,
+        @Min(1) int replicationFactor,
+        long retentionMs,
+        String cleanupPolicy
+    ){}
+}
